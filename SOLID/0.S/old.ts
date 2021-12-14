0dc22352638fe8853d017055c1574a47eca80213
unknown <nehalonkar27@gmail.com>
2021-12-14 10:31:18 +0100

    refactor code as per single resoponsibility principle

---
+++ SOLID/0.S/old.ts
@@ -0,0 +1,55 @@
+// import{Engine} from "./engine";
+export class Car {
+    //it is convention to start property names in TypeScript with an underscore.
+    // If you want to known why, remove the underscore and see if your compiler is throwing you an error!
+    
+    private _fuel : number = 0;
+    private _miles : number = 0;
+   
+
+    //By changing this variable to readonly I have in essence created a property constant.
+    // the only subtle difference is that you can write once to the variable inside the constructor
+    private readonly MAXIMUM_FUEL_CAPACITY: number;
+    private readonly FUEL_MILEAGE: number = 10;
+
+    constructor(MAXIMUM_FUEL_CAPACITY: number) {
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+    }
+
+    get miles(): number {
+        return this._miles;
+    }
+
+//Take attention to these getter and setters
+   
+
+    get fuel(): number {
+        return this._fuel;
+    }
+
+    //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
+    // it is better to provide a specific method for this instead of a generic setter.
+    // with a setter there is always the chance of somebody lowering the fuel amount by accident.
+    addFuel(fuel : number) {
+        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
+    }
+
+   
+
+    drive() {
+        if(this.engineStatus === false || this._fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        
+        this._fuel -= 1;
+        this._miles += this.FUEL_MILEAGE;
+    }
+}
+
+// When you see <cast>variable this is a "cast" of a variable, explicitly telling the code what the type of this variable will be.
+// This is sometimes needed when a default JS function does not return a precise enough Type.
+// I need to cast this to HtmlElement because the default Element return type is not specific to the HTML context (because some versions of JS can also be used in the backend, see node.js)
+// This makes it not having some properties like .innerText. Test it out yourself by removing the <HTMLElement>
